07c73ab42fb49069ece8a755100dcf225b1db95e
Olivier Filangi <olivier.filangi@inrae.fr>
2024-04-08 21:58:40 +0200

    save dev

--- Readme.md
+++ Readme.md
@@ -1,16 +1,19 @@
 
-# Install
+## App server 
+
+### Install
 ```
-python -m venv env
-. .venv/bin/activate
-pip install -r ./requirements.txt
+python3 -m venv env
+. env/bin/activate
+pip3 install flask docker python-dotenv
+
 ```
 
-## config
+### config
 
 edit `.flaskenv`
 
-# Run service
+### Run service
 
 ```bash
 . .venv/bin/activate
@@ -18,3 +21,47 @@ flask run
 ```
 
 https://services-p2m2-test-192-168-100-66.vm.openstack.genouest.org
+
+## Docker images
+
+- https://hub.docker.com/r/inraep2m2/thermorawfileparser
+
+### inraep2m2/Dockerfile_thermorawfileparser
+
+*No github repository*
+version : ThermoRawFileParser1.4.3.zip
+
+#### Build and push image
+
+```bash
+docker build . -f Dockerfile_thermorawfileparser -t inraep2m2/thermorawfileparser:1.4.3
+docker login --username=p2m2
+docker image push inraep2m2/thermorawfileparser:1.4.3
+```
+
+#### Usage
+
+```bash
+raw data is localized in $PWD/data directory
+docker run -v $PWD/data:/data -t inraep2m2/thermorawfileparser:1.4.3 -i=/data/MM_NOx_1_Direct.raw
+```
+
+### inraep2m2/openms
+
+- https://raw.githubusercontent.com/OpenMS/dockerfiles/master/executables/Dockerfile
+
+#### Build and push image
+
+```bash
+docker build . -f Dockerfile_OpenMS -t inraep2m2/openms:3.1.0-pre-nightly-2024-02-03
+docker login --username=p2m2
+docker image push inraep2m2/openms:3.1.0-pre-nightly-2024-02-03
+```
+
+#### Usage
+
+```bash
+raw data is localized in $PWD/data directory
+docker run -v $PWD/data:/data -it inraep2m2/openms:3.1.0-pre-nightly-2024-02-03 FileConverter -in /data/MM_NOx_1_Direct.mzML -out /data/MM_NOx_1_Direct.mzXML
+```
+
--- app.py
+++ app.py
@@ -12,6 +12,7 @@ UPLOAD_FOLDER = '/tmp/'
 ## UPDATE HERE IMAGE
 ## ===============================
 docker_image_thermorawfileparser = 'inraep2m2/thermorawfileparser:1.4.3'
+docker_image_openms              = 'inraep2m2/openms:3.1.0-pre-nightly-2024-02-03'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -20,6 +21,8 @@ container = None
 itnum=1
 dirworkpath=None
 diroutputpath=None 
+
+format="mzML"
 
 @app.route('/')
 def index():
@@ -31,7 +34,8 @@ def logs():
     global itnum
     global diroutputpath
     global dirworkpath
-    
+    global format 
+
     if container is None:
         return jsonify(console="Waiting for Docker container ...",run=True)
     else:
@@ -48,12 +52,16 @@ def logs():
         except:
             
             run = False
+            ## load openms to convert file
+            client = docker.from_env()
+            client.images.pull( docker_image_openms ) 
+            diroutputpath_withnewformat = tempfile.mkdtemp()
 
             ## Hack change Voc MS:1003145 (ThermoRawFileParser to MsConvert) to MS:1000615
             
             totall = "<h3><i>Hack W4M</i></h3>"
             totall += "MS:1003145 (ThermoRawFileParser) -> MS:1000615 (MsConvert)<br/>"
-            
+
             for file in glob.glob(diroutputpath+"/*.mz*"):
                    
                 with open(file, 'r') as f:
@@ -70,12 +78,29 @@ def logs():
                     f.close()
                     totall+='<span style="color:red;">' + file.split("/")[-1] + "</span><br/>"
                 
+                filename = os.path.basename(file)
+                filename_without_ext = os.path.splitext(filename)[0]
+                print("-in=/data/{} --out=/output/{}".format(filename,filename_without_ext+"."+format))
+                print("===========================")
+                if format !=  "mzML" :
+                    totall = "<h3>Docker console ({})</h3>".format(docker_image_openms)
+                    totall += '<span style="color:green;">' + (client.containers.run(docker_image_openms,
+                                "FileConverter -in /data/{} -out /output/{}".format(filename,filename_without_ext+"."+format),                            
+                                volumes={
+                                     diroutputpath : {'bind': '/data/', 'mode': 'rw'},
+                                     diroutputpath_withnewformat : {'bind': '/output/', 'mode': 'rw'},
+                                },
+                                detach=False,
+                                remove=False)).decode("utf-8").replace("\n","<br/>") +"</span><br/>"
+                
             ## make archive
-
-            shutil.make_archive(app.config['UPLOAD_FOLDER']+'/data', 'zip', diroutputpath)
-            
+            if format !=  "mzML" :
+                shutil.make_archive(app.config['UPLOAD_FOLDER']+'/data', 'zip', diroutputpath_withnewformat)
+            else:
+                shutil.make_archive(app.config['UPLOAD_FOLDER']+'/data', 'zip', diroutputpath)
+                        
             # delete unuserd directories/files
-
+            shutil.rmtree(diroutputpath_withnewformat)
             shutil.rmtree(diroutputpath)
             shutil.rmtree(dirworkpath)
             
@@ -96,6 +121,8 @@ def download_results():
 def process():
     # Handle form submission and file processing here
     if request.method == "POST":
+        global format
+
         file = request.files['file']
         format = request.form['format']
         print(file)
